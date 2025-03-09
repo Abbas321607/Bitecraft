834918a6996ac62c533fd48be4d50141298352e4
abbas321607 <zaheerabbassyed64@gmail.com>
2025-03-09 17:03:49 +0530

    new updates

--- frontend/src/Components/ui/Navbar.js
+++ frontend/src/Components/ui/Navbar.js
@@ -1,20 +1,40 @@
-import  { Navbar, Nav, Container } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
 function CollapsibleExample() {
+  const { isLoggedIn, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
+
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" fixed="top">
       <Container>
+        <Navbar.Brand as={NavLink} to="/" className="fw-bold">BITE CRAFT</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="./">Home</Nav.Link> 
-            {/* <Nav.Link href="./">create recipe</Nav.Link>       */}
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            {isLoggedIn ? (
+              <>
+                <Nav.Link as={NavLink} to="/create-recipe">Create Recipe</Nav.Link>
+                <Nav.Link as={NavLink} to="/favorites">Favorites</Nav.Link>
+              </>
+            ) : null}
           </Nav>
           <Nav>
-            <Nav.Link as={NavLink} to="./pages/login">Login</Nav.Link>
-            <Nav.Link eventKey={2} as={NavLink} to="./pages/register">Register</Nav.Link>
+            {!isLoggedIn ? (
+              <>
+                <Nav.Link as={NavLink} to="/pages/login">Login</Nav.Link>
+                <Nav.Link as={NavLink} to="/pages/register">Register</Nav.Link>
+              </>
+            ) : (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,25 +1,34 @@
-import React  from 'react'
-import Navbar from './Components/ui/Navbar'
+import React from 'react';
+import Navbar from './Components/ui/Navbar';
 
+const Header = ({ names, inputChange, searchRecipe }) => {
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      searchRecipe();
+    }
+  };
 
-const Header = ({names, inputChange, searchRecipe}) => {
-   
-    return (
-        <>
-            <div className="jumbotron">
-                <div className="tittle">
-                <Navbar/>
-                    <h1 className="display-1 text-center">Bite Craft</h1>
-                    </div>
-                <div class="input-group w-50 mx-auto">
-                    <input type="text" class="form-control" placeholder="Search Your Recipe" value={names} onChange={inputChange} />
-                    <div className="input-group-append">
-                        <button className="btn btn-dark" onClick={()=>{searchRecipe()}}>Search </button>
-                    </div>
-
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <div className="jumbotron">
+        <h1 className="display-1">BITE CRAFT</h1>
+        <div className="input-group w-50 mx-auto">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search Recipe..."
+            value={names}
+            onChange={inputChange}
+            onKeyPress={handleKeyPress}
+          />
+          <button className="btn btn-dark" onClick={searchRecipe}>
+            Search
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
+
 export default Header;
